b647cf773c9cb1bf17a4f1e19e7938393f6a2ba6
Max Shenfield <shenfieldmax@gmail.com>
2017-12-02 16:26:59 -0600

    Give correct shell command to write iterm2 defaults and add back in all keys

--- .bootstrap/_defaults.sh
+++ .bootstrap/_defaults.sh
@@ -287,7 +287,7 @@ sudo mdutil -E / > /dev/null
 
 # Copy over custom color settings, profiles, and terminal wide defaults
 # HACK: Dump the result of running `defaults read com.google.iterm2` on my current machine
-defaults write com.google.iterm2 \'$(cat ~/.dotfiles/.bootstrap/_iterm2.defaults)\'
+defaults write com.google.iterm2 -dict "$(cat ~/.dotfiles/.bootstrap/_iterm2.defaults)"
 
 ###############################################################################
 # TextEdit                                                                    #
